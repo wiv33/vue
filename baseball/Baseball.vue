65e15859b39e6609cfb6c1abc219a58dfa308eae
PSK <wiv.psk@gmail.com>
2019-12-08 01:43:39 +0900

    no message

--- baseball/Baseball.vue
+++ baseball/Baseball.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
-            <input type="text" ref="answer" maxlength="4" v-model="value">
+        <form @submit.prevent="onSubmitForm">
+            <input type="text" ref="answer" minlength="4" maxlength="4" v-model="value">
             <button>입력</button>
         </form>
-        <div>시도 : {{}}</div>
+        <div>시도 : {{tries.length}}</div>
+        <ul>
+            <li v-for="t in tries">
+                <div>{{ t.try }}</div>
+                <div>{{ t.result }}</div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,13 +19,19 @@
     export default {
         data() {
             return {
+                tries: [],
                 value: '',
                 result: ''
             }
         },
         methods: {
             onSubmitForm(e) {
-                e.preventDefault();
+                this.tries.push({
+                    try: this.value,
+                    result: '홈런'
+                });
+                this.$refs.answer.focus();
+                this.value = '';
             }
         }
     }
