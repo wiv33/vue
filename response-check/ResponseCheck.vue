ecf5d1a9f495da08afaaef7411b2d1a124f2ce86
PSK <wiv.psk@gmail.com>
2019-12-08 15:11:40 +0900

    result

--- response-check/ResponseCheck.vue
+++ response-check/ResponseCheck.vue
@@ -2,7 +2,7 @@
     <div>
         <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
         <div>
-            <div>평균시간: {{}}</div>
+            <div>평균시간: {{result.reduce((a,b) => a + b, 0) / result.length || 0}}ms</div>
         </div>
         <button @click="onReset">리셋</button>
     </div>
@@ -19,18 +19,30 @@
         },
         methods: {
             onReset() {
-
+                this.result = [];
             },
             onClickScreen() {
+                let startTime = 0, endTime = 0, timeout = null;
                 switch (this.state) {
                     case "waiting":
                         this.state = 'ready';
+                        this.message = '초록색이 되면 클릭하세요.';
+                        timeout = setTimeout(() => {
+                            this.state = 'now';
+                            this.message = '지금 클릭!';
+                            startTime = new Date().getMilliseconds();
+                        }, Math.floor(Math.random() * 1000) + 2000) // 2 ~ 3초
                         break;
                     case "ready":
-                        this.state = 'now';
+                        clearTimeout(timeout);
+                        this.state = 'waiting';
+                        this.message = '너무 빨리 클릭했습니다. 초록색이 된 후 클릭하세요.'
                         break;
                     case "now":
                         this.state = 'waiting';
+                        this.message = '클릭해서 시작하세요.';
+                        endTime = new Date().getMilliseconds();
+                        this.result.push(endTime - startTime);
                         break;
                 }
             }
