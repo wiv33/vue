65e6511499c2ac2dd6cb8e963f8c86a9f6abdf59
PSK <wiv.psk@gmail.com>
2019-12-11 00:09:13 +0900

    add EventBus.vue

--- tic-tak-toe/TdComponent.vue
+++ tic-tak-toe/TdComponent.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+    import EventBus from "./EventBus";
+
     export default {
         name: "TdComponent",
         created() {
@@ -15,54 +17,9 @@
         },
         methods: {
             onClickTd() {
-                if (this.cellData) return ;
-
-                // console.log(this.$root.$data);
-                const rootData = this.$root.$data;
-                this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
-
-                let win = false;
-                if (rootData.tableData[this.rowIndex][0] === rootData.turn
-                    && rootData.tableData[this.rowIndex][1] === rootData.turn
-                    && rootData.tableData[this.rowIndex][2] === rootData.turn) {
-                    win = true;
-                } else if (rootData.tableData[0][this.cellIndex] === rootData.turn
-                    && rootData.tableData[1][this.cellIndex] === rootData.turn
-                    && rootData.tableData[2][this.cellIndex] === rootData.turn) {
-                    win = true;
-                } else if (rootData.tableData[0][0] === rootData.turn
-                    && rootData.tableData[1][1] === rootData.turn
-                    && rootData.tableData[2][2] === rootData.turn) {
-                    win = true;
-                } else if (rootData.tableData[0][2] === rootData.turn
-                    && rootData.tableData[1][1] === rootData.turn
-                    && rootData.tableData[2][0] === rootData.turn) {
-                    win = true;
-                }
-
-                if (win) {
-                    rootData.winner = rootData.turn;
-                    rootData.turn = 'O';
-                    rootData.tableData = [['', '', ''],['', '', ''],['', '', '']]
-                } else { // 무승부
-                    let all = true; //all이 무승부라는 뜻
-                    rootData.tableData.forEach(row => {
-                        row.forEach(cell => {
-                            if (!cell) {
-                                all = false;
-                            }
-                        });
-                    });
-
-                    if (all) {
-                        rootData.winner = '';
-                        rootData.turn = 'O';
-                        rootData.tableData = [['', '', ''],['', '', ''],['', '', '']]
-                    } else {
-                        rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
-                    }
-
-                }
+                if (this.cellData) return;
+                //emit이 되면 on이 실행된다.
+                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
             }
         }
     }
--- tic-tak-toe/TicTacToe.vue
+++ tic-tak-toe/TicTacToe.vue
@@ -8,6 +8,8 @@
 
 <script>
     import TableComponent from "./TableComponent";
+    import EventBus from "./EventBus";
+
     export default {
         name: "TicTacToe",
         components: {
@@ -28,9 +30,59 @@
             onChangeData() {
                 // this.tableData[1][0] = 'X'; 작동하지 않음
                 this.$set(this.tableData[1], 0, 'X'); // Vue.set 동일
+            },
+            onClickTd(rowIndex, cellIndex) {
+                // console.log(this.$root.$data);
+                this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+
+                let win = false;
+                if (this.tableData[rowIndex][0] === this.turn
+                    && this.tableData[rowIndex][1] === this.turn
+                    && this.tableData[rowIndex][2] === this.turn) {
+                    win = true;
+                } else if (this.tableData[0][cellIndex] === this.turn
+                    && this.tableData[1][cellIndex] === this.turn
+                    && this.tableData[2][cellIndex] === this.turn) {
+                    win = true;
+                } else if (this.tableData[0][0] === this.turn
+                    && this.tableData[1][1] === this.turn
+                    && this.tableData[2][2] === this.turn) {
+                    win = true;
+                } else if (this.tableData[0][2] === this.turn
+                    && this.tableData[1][1] === this.turn
+                    && this.tableData[2][0] === this.turn) {
+                    win = true;
+                }
+
+                if (win) {
+                    this.winner = this.turn;
+                    this.turn = 'O';
+                    this.tableData = [['', '', ''],['', '', ''],['', '', '']]
+                } else { // 무승부
+                    let all = true; //all이 무승부라는 뜻
+                    this.tableData.forEach(row => {
+                        row.forEach(cell => {
+                            if (!cell) {
+                                all = false;
+                            }
+                        });
+                    });
+
+                    if (all) {
+                        this.winner = '';
+                        this.turn = 'O';
+                        this.tableData = [['', '', ''],['', '', ''],['', '', '']]
+                    } else {
+                        this.turn = this.turn === 'O' ? 'X' : 'O';
+                    }
+
+                }
+
             }
         },
-
+        created() {
+            EventBus.$on('clickTd', this.onClickTd);
+        }
     }
 </script>
 
